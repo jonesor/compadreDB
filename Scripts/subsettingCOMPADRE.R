d25c687495b594295076e487cb73df0457ed47e8
Owen Jones <jonesor@gmail.com>
2014-07-21 17:58:07 +0200

    Formatting
    Minor formatting tweaks.

--- Scripts/subsettingCOMPADRE.R
+++ Scripts/subsettingCOMPADRE.R
@@ -1,15 +1,27 @@
-#Set your working directory, then load the COMPADRE data:
+# ---------------------------------------------------------------------------- #
+# - DESCRIPTION: Code to demonstrate subsetting of the COMPADRE database to 
+#                species of interest.
+# - AUTHORS:     Owen Jones
+# ---------------------------------------------------------------------------- #
+
+# Set the working directory, then load the COMPADRE data:
 load("COMPADRE Jul 21 2014.RData")
 
-#Subsetting to data of interest:
-#I want to look at the mean matrrices for shrubs that are from studies that are 5+ years long, with a dimension of 5+.
-#I first use subset() to subset the metadata part of the compadre object to rows that match myn criteria. 
-tempMetadata <- subset(compadre$metadata,MatrixComposite == "Mean" & GrowthType =="Shrub" & StudyDuration >= 5 & MatrixDimension >=4)
+# Subsetting to data of interest:
+# I want to look at the mean matrrices for shrubs that are from studies that are 
+# 5+ years long, with a dimension of 5+.
+# I first use subset() to subset the metadata part of the compadre object to rows 
+# that match myn criteria. 
 
-#Now I can use the row names from the subsetted dataframe to subset the matrices.
+tempMetadata <- subset(compadre$metadata,MatrixComposite == "Mean" & GrowthType =="Shrub" & StudyDuration >= 5 & MatrixDimension >= 4)
+
+# Now I can use the row names from the subsetted dataframe to subset the matrices.
+
 keep <- rownames(tempMetadata)
 tempMat <- compadre$mat[keep]
 
-#The object tempMat is now a list object containing matrices in the same order that their metadata appears in tempMetadata
-#I could analyse these matrices by using a loop, or by using lapply.
+# The object tempMat is now a list object containing matrices in the same order 
+# that their metadata appears in tempMetadata.
+# I could analyse these matrices by applying functions in a loop, or by using 
+# lapply.
 
