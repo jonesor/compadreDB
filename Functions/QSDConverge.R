216d32cc89403403fa65d90192b594413be00e07
Owen Jones <jones@biology.sdu.dk>
2014-08-24 13:27:43 +0100

    Changed name of function to be shorter

--- Functions/QSDConverge.R
+++ Functions/QSDConverge.R
@@ -1,4 +1,4 @@
-qsdConvergence <- function(matU, conv = 0.05, startLife = 1, nSteps = 1000){
+qsdConverge <- function(matU, conv = 0.05, startLife = 1, nSteps = 1000){
   #Function to determine the cutoff age at quasi-convergence for lx and mx (Code adapted from H. Caswell's matlab code):
   
   uDim = dim(matU)
