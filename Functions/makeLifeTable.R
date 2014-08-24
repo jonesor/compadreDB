638a36b156371990dd8c00607dca447dba43972b
Owen Jones <jones@biology.sdu.dk>
2014-08-24 13:09:20 +0100

    makeLifeTable function lx and mx

--- Functions/makeLifeTable.R
+++ Functions/makeLifeTable.R
@@ -1,5 +1,33 @@
-makeLifeTable(matU, matF = NULL, matC = NULL, start = 1, nsteps = 1000){
+makeLifeTable<-function(matU, matF = NULL, matC = NULL, startLife = 1, nSteps = 1000){
+  
+  matDim = ncol(matU)
+  #Age-specific survivorship (lx) (See top function on page 120 in Caswell 2001):
+  matUtemp = matU
+  survivorship = array(NA, dim = c(nsteps, matDim))
+  for (o in 1:nSteps){
+    survivorship[o, ] = colSums(matUtemp %*% matU)
+    matUtemp = matUtemp %*% matU
+  }
+  
+  lx = survivorship[, startLife]
+  lx = c(1, lx[1:(length(lx) - 1)])
+  
+  #Age-specific fertility (mx, Caswell 2001, p. 120)
+  ageFertility = array(0, dim = c(nSteps, matDim))
+  fertMatrix = array(0, dim = c(nSteps, matDim))
+  matUtemp2 = matU
+  e = matrix(rep(1, matDim))
+  for (q in 1:nSteps) {
+    fertMatrix = matF %*% matUtemp2 * (as.numeric((ginv(diag(t(e) %*% matUtemp2)))))
+    ageFertility[q, ] = colSums(fertMatrix)
+    Umat3 = Umat3 %*% Umat
+  }  
+  mx = ageFertility[, notProp]
   
   
   
-}
+  
+  #Assemble output object
+  out = data.frame(x = 0:(length(lx)-1),lx = lx)
+  return(out)
+  }
