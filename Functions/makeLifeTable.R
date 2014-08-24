473ba415b8fe7355a5b5fb12f795849771ee16a4
robcito <r.salguero@uq.edu.au>
2014-08-24 15:24:40 +0100

    Age specific clonality
    Added cx functions and warnings when matF and matC are all zeros

--- Functions/makeLifeTable.R
+++ Functions/makeLifeTable.R
@@ -16,7 +16,10 @@ makeLifeTable<-function(matU, matF = NULL, matC = NULL, startLife = 1, nSteps = 
   #Start to assemble output object
   out = data.frame(x = 0:(length(lx)-1),lx = lx)
   
-  if(missing(matF) == FALSE){
+  if(!missing(matF)){
+    if(sum(matF,na.rm=T)==0){
+      warning("matF contains only 0 values")
+    }
   #Age-specific fertility (mx, Caswell 2001, p. 120)
   ageFertility = array(0, dim = c(nSteps, matDim))
   fertMatrix = array(0, dim = c(nSteps, matDim))
@@ -30,7 +33,25 @@ makeLifeTable<-function(matU, matF = NULL, matC = NULL, startLife = 1, nSteps = 
   mx = ageFertility[, startLife]
   mx = c(0, mx[1:(length(mx) - 1)])
   out$mx = mx
+  }
   
+  if(!missing(matC)){
+     if(sum(matC,na.rm=T)==0){
+      warning("matC contains only 0 values")
+    }
+  #Age-specific clonality (cx)
+  ageClonality = array(0, dim = c(nSteps, matDim))
+  clonMatrix = array(0, dim = c(nSteps, matDim))
+  matUtemp2 = matU
+  e = matrix(rep(1, matDim))
+  for (q in 1:nSteps) {
+    clonMatrix = matC %*% matUtemp2 * (as.numeric((ginv(diag(t(e) %*% matUtemp2)))))
+    ageClonality[q, ] = colSums(clonMatrix)
+    matUtemp2 = matUtemp2 %*% matU
+  }  
+  cx = ageClonality[, startLife]
+  cx = c(0, cx[1:(length(cx) - 1)])
+  out$cx = cx
   }
   
   if(missing(matC) == FALSE){
