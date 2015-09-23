8175cd42ef266e961e0157658777d12b45d8bdfc
Owen Jones <jones@biology.sdu.dk>
2015-09-23 16:32:04 +0200

    Added conversion for U, F, and C matrix, and the stage names

--- Functions/convert2flat.R
+++ Functions/convert2flat.R
@@ -2,15 +2,19 @@
 # used by Dave Hodgson's group in University of Exeter.
 #
 # use: x<-convert2flat(compadre)
+# write.csv(x,file = "compadreFlat.csv",row.names=FALSE)
+
 
 convert2flat <- function(db=compadre, Aonly = TRUE){
  
   db$metadata$Amatrix <- NULL
   for (i in 1:nrow(db$metadata)){
-    db$metadata$Amatrix[i] <- paste("[",paste(db$mat[[i]][[1]],collapse=" "),"]",sep="")
+    db$metadata$classnames[i] <- paste(db$matrixClass[[i]]$MatrixClassAuthor,collapse=", ")
+    db$metadata$matrixA[i] <- paste("[",paste(db$mat[[i]]$matA,collapse=" "),"]",sep="")
+    db$metadata$matrixU[i] <- paste("[",paste(db$mat[[i]]$matU,collapse=" "),"]",sep="")
+    db$metadata$matrixF[i] <- paste("[",paste(db$mat[[i]]$matF,collapse=" "),"]",sep="")
+    db$metadata$matrixC[i] <- paste("[",paste(db$mat[[i]]$matC,collapse=" "),"]",sep="")
   }
  
   return(db$metadata)
 }
-
-
