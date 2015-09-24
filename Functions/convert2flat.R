9bd7415a59ccaa73c1b48423e6bc41409416740e
Owen Jones <jones@biology.sdu.dk>
2015-09-24 13:46:16 +0200

    Updated convert2flat.R to use a pipe symbol (|) as the separator for classnames.
    Using a comma is not possible because the classnames also has commas in
    occasionally.

--- Functions/convert2flat.R
+++ Functions/convert2flat.R
@@ -9,7 +9,7 @@ convert2flat <- function(db=compadre, Aonly = TRUE){
  
   db$metadata$Amatrix <- NULL
   for (i in 1:nrow(db$metadata)){
-    db$metadata$classnames[i] <- paste(db$matrixClass[[i]]$MatrixClassAuthor,collapse=", ")
+    db$metadata$classnames[i] <- paste(db$matrixClass[[i]]$MatrixClassAuthor,collapse="| ")
     db$metadata$matrixA[i] <- paste("[",paste(db$mat[[i]]$matA,collapse=" "),"]",sep="")
     db$metadata$matrixU[i] <- paste("[",paste(db$mat[[i]]$matU,collapse=" "),"]",sep="")
     db$metadata$matrixF[i] <- paste("[",paste(db$mat[[i]]$matF,collapse=" "),"]",sep="")
