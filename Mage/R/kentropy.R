daf6db3483b11a929927a123375065bcb0cc6a84
Owen Jones <jones@biology.sdu.dk>
2016-06-08 12:18:08 +0200

    Added Keyfitz Entropy function, and Rd file.

---
+++ Mage/R/kentropy.R
@@ -0,0 +1,14 @@
+kentropy <- function(lx, trapeze = TRUE){
+  
+  if(max(lx) > 1) stop("`lx` should be bounded between 0 and 1")
+  if(sum(is.na(lx))>1) stop("There are missing values in `lx`")
+  if(sum(!diff(lx)) > 0) stop("`lx` does not monotonically decline")
+
+  if(trapeze == TRUE){
+    ma <- function(x,n=2){filter(x,rep(1/n,n), sides=2)}
+    lx2 <- na.omit(as.vector(ma(lx)))
+    return(-sum(lx2*log(lx2))/sum(lx2))
+    }else{
+      return(-sum(lx*log(lx))/sum(lx))
+    }
+  }
