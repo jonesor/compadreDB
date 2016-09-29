f61b3b8a9ea7bec5fbdd0999a63596da6474becb
Owen Jones <jones@biology.sdu.dk>
2016-09-29 11:01:53 +0200

    Tweaked entry code to warn if lx (survivorship) is flat (no deaths)

--- Mage/R/kentropy.R
+++ Mage/R/kentropy.R
@@ -4,8 +4,10 @@ kentropy <- function(lx, trapeze = TRUE){
   
   if(max(lx) > 1) stop("`lx` should be bounded between 0 and 1")
   if(sum(is.na(lx))>1) stop("There are missing values in `lx`")
-  if(sum(!diff(lx)) > 0) stop("`lx` does not monotonically decline")
-
+  #if(sum(!diff(lx) <= 0)) stop("`lx` does not monotonically decline")
+  if(sum(!diff(lx) <= 0)!=0)stop("`lx` does not monotonically decline")
+  
+  
   if(trapeze == TRUE){
     ma <- function(x,n=2){filter(x,rep(1/n,n), sides=2)}
     lx2 <- na.omit(as.vector(ma(lx)))
