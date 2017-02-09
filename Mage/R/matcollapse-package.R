62d360c138c88e999931eaffd0d3ab9ffd09ba6b
Scott Chamberlain <myrmecocystus@gmail.com>
2017-02-09 18:27:58 +0100

    started collapse matrix fxns

--- Mage/R/matcollapse-package.R
+++
@@ -1,8 +0,0 @@
-#' Collapse matrices
-#'
-#' @name matcollapse-package
-#' @aliases matcollapse
-#' @docType package
-#' @author Scott Chamberlain \email{myrmecocystus@@gmail.com}
-#' @keywords package
-NULL
